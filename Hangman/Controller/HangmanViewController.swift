dfe928365616e6a63d7230664728ad6504aeca05
Elden Li <lielden@yahoo.com>
2019-02-27 20:02:22 -0800

    added some shit

--- Hangman/Controller/HangmanViewController.swift
+++ Hangman/Controller/HangmanViewController.swift
@@ -14,7 +14,7 @@ class HangmanViewController: UIViewController {
     @IBOutlet weak var puzzleWord: UILabel!
     
     var count: Int = 1
-    let game = Game()
+    var game = Game()
     
     let startImage = UIImage(named: "hangman1")
     
@@ -22,15 +22,6 @@ class HangmanViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         hangmanImage.image = startImage
-        
-    }
-    
-    var phrases : NSArray!
-    
-    init() {
-        // Replace "phrases" with your plist's filename
-        let path = Bundle.main.path(forResource: "phrases", ofType: "plist")
-        phrases = NSArray.init(contentsOfFile: path!)
     }
     
     @IBOutlet weak var guessField: UITextField!
@@ -58,12 +49,16 @@ class HangmanViewController: UIViewController {
         restart()
     }
     
+    func checkLetter(_ letter: String) -> Bool {
+        if (game.word.contains(letter)) {
+            return true
+        }
+        return false
+    }
     
     func gameOver(_ rightAnswer: String) {
         let alertController = UIAlertController(title: "You Lost", message: "The right answer was " + rightAnswer, preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "Restart", style: UIAlertAction.Style.default, handler: nil))
         self.present(alertController, animated: true, completion: nil)
     }
-    
-    
 }
