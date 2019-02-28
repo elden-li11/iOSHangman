20e5e915c9636aec776260b712db384f6a96cbb9
Elden Li <lielden@yahoo.com>
2019-02-27 17:25:22 -0800

    changed view controller

--- Hangman/Controller/HangmanViewController.swift
+++ Hangman/Controller/HangmanViewController.swift
@@ -11,19 +11,37 @@ import UIKit
 class HangmanViewController: UIViewController {
     
     @IBOutlet weak var hangmanImage: UIImageView!
-
+    
+    var count: Int = 1
+    let startImage = UIImage(named: "hangman1")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let startImage = UIImage(named: "hangman1")
         hangmanImage.image = startImage
     }
     
-
+    
     @IBOutlet weak var guessField: UITextField!
     
     @IBAction func guessPressed(_ sender: Any) {
-        
+        self.count += 1
+        hangmanImage.image = UIImage(named: "hangman" + String(count))
+        if (count == 7) {
+            gameOver("Cheesecake")
+            restart()
+        }
+    }
+    
+    func restart() {
+        count = 0
+        hangmanImage.image = startImage
+    }
+    
+    func gameOver(_ rightAnswer: String) {
+        let alertController = UIAlertController(title: "You Lost", message: "The right answer was " + rightAnswer, preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Restart", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
     }
 }
 
