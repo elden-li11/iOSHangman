b18b7bb515f3f0793540cded3707337b1c00c874
Arman <arman.vaziri1@gmail.com>
2019-02-27 19:26:21 -0800

    restart button implemented

--- Hangman/Controller/HangmanViewController.swift
+++ Hangman/Controller/HangmanViewController.swift
@@ -21,14 +21,11 @@ class HangmanViewController: UIViewController {
         hangmanImage.image = startImage
     }
     
-    
     @IBOutlet weak var guessField: UITextField!
     
     func textFieldBeganEditing(textField: UITextField) {
         
     }
-    
-    
     
     @IBAction func guessPressed(_ sender: Any) {
         self.count += 1
@@ -44,10 +41,17 @@ class HangmanViewController: UIViewController {
         hangmanImage.image = startImage
     }
     
+    //Resets the game.
+    @IBAction func restartButtonPressed(_ sender: UIButton) {
+        restart()
+    }
+    
+    
     func gameOver(_ rightAnswer: String) {
         let alertController = UIAlertController(title: "You Lost", message: "The right answer was " + rightAnswer, preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "Restart", style: UIAlertAction.Style.default, handler: nil))
         self.present(alertController, animated: true, completion: nil)
     }
+    
+    
 }
-
