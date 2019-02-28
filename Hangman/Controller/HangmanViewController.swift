879ce3da97e5b75a80fd9263fc1f353c5d383312
Arman <arman.vaziri1@gmail.com>
2019-02-27 21:51:05 -0800

    actually done for the night

--- Hangman/Controller/HangmanViewController.swift
+++ Hangman/Controller/HangmanViewController.swift
@@ -44,16 +44,18 @@ class HangmanViewController: UIViewController {
         }
     }
     
+    //Restarts the game
     func restart() {
         game.counter = 0
         hangmanImage.image = startImage
     }
     
-    //Resets the game.
+    //Resets the game upon the restart button press
     @IBAction func restartButtonPressed(_ sender: UIButton) {
         restart()
     }
     
+    //Checks if the users input matches a letter in the phrase
     func checkLetter(_ letter: String) -> Bool {
         if (game.word.contains(letter)) {
             return true
@@ -61,12 +63,14 @@ class HangmanViewController: UIViewController {
         return false
     }
     
+    //When the user guesses wrong 7 times
     func gameOver(_ rightAnswer: String) {
         let alertController = UIAlertController(title: "You Lost", message: "The right answer was " + game.word + ".", preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "Restart", style: UIAlertAction.Style.default, handler: nil))
         self.present(alertController, animated: true, completion: nil)
     }
     
+    //Alert for when the user inputs an invalid guess
     func invalidInputAlert() {
         let alertController = UIAlertController(title: "Invalid Input", message: "You must enter one single letter.", preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "Continue", style: UIAlertAction.Style.default, handler: nil))
@@ -79,6 +83,7 @@ class HangmanViewController: UIViewController {
         self.present(alertController, animated: true, completion: nil)
     }
     
+    //Does shit when the user inputs an incorrect guess
     func wrongLetterInputed(_ letter: String) {
         if (game.incorrectGuesses.contains(letter)) {
             repeatedInputAlert()
@@ -86,7 +91,6 @@ class HangmanViewController: UIViewController {
             if game.loseAGuess() == false {
                 hangmanImage.image = UIImage(named: "hangman" + String(game.counter))
                 gameOver(game.word)
-               
             } else {
                 hangmanImage.image = UIImage(named: "hangman" + String(game.counter))
             }
